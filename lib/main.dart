e5fc8f6c7881bc9780e17064226df478f9222e30
Patrick of AdDU <patrick.paasa@gmail.com>
2023-01-03 09:15:48 +0800

    Gradient login on main

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,6 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -24,45 +26,59 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'IT Elective 5', number: 1080,),
+      home:  const LoginScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title, required this.number});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-  final double number;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-       appBar: AppBar(
-        title: Text(widget.title),
-       ),
-       body: Container(
+      
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Color(0xFFc3199b),
+              Color(0xFF294085),
+            ]
+          )
+        ),
         width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
-        color: Colors.blue[900],
-        child: const Center(child: Text("Hello World", style: TextStyle(color: Colors.white),),),
-       ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            theButton("Login to Google", MediaQuery.of(context).size.width),
+            const SizedBox(height:15,),
+            theButton("Login to Twitter", MediaQuery.of(context).size.width),
+            const SizedBox(height:15,),
+            theButton("Login to Facebook", MediaQuery.of(context).size.width),
+          ],
+        ),
+      )
+    );
+  }
+
+  Widget theButton(String caption, double width){
+    return Container(
+      width: width * .75,
+      child: ElevatedButton(
+        onPressed: () {
+          
+        },
+        
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all<Color>(Colors.blue),
+          padding: MaterialStateProperty.all<EdgeInsets>(EdgeInsets.all(15.0))
+        ),
+        child: Text(caption, style: const TextStyle(fontSize: 20),),
+      ),
     );
   }
 }
